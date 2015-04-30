ff9e746d55d3e256468eb95a768afd8626b3396f
Andreas Bissinger <mail@bissinger-andreas.de>
2015-04-30 19:09:47 +0200

    Create models Person, Birthdate, Birthday

---
+++ oc/models.py
@@ -0,0 +1,16 @@
+class Person:
+    def __init__(self, name, birthdate):
+        self.name = name
+        self.birthdate = birthdate
+
+
+class Birthdate:
+    def __init__(self, year, day):
+        self.year = year
+        self.day = day
+
+
+class Day:
+    def __init__(self, day, month):
+        self.day = day
+        self.month = month
