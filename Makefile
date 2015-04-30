c0e65be94f2f978509dfd9173134dee52c8fb9ba
Andreas Bissinger <mail@bissinger-andreas.de>
2015-04-30 19:03:22 +0200

    Create oc-package, Makefile and class template

---
+++ Makefile
@@ -0,0 +1,5 @@
+class:
+	cp template_class.py oc/new_class.py
+
+rules:
+	cat README.md
---
+++ template_class.py
@@ -0,0 +1,4 @@
+class CLASS:
+    __init__(self, ATTR_A, ATTR_B):
+        self.ATTR_A = ATTR_A
+        self.ATTR_B = ATTR_B
