bbb3119c0087ec52185cd275b5dc132868129658
Andreas Bissinger <mail@bissinger-andreas.de>
2015-04-30 19:38:19 +0200

    Create Calender with list of all dates

--- oc/models.py
+++ oc/models.py
@@ -1,3 +1,25 @@
+class Calendar:
+    def __init__(self, year=2015):
+        self.year = year  # TODO get current year
+        self.dates = []
+        for month in range(1, 13):
+            self.insert_dates(month)
+
+    def insert_dates(self, month):
+        days = 28
+        if month in [1, 4, 6, 9, 11]:
+            days = 30
+        if month in [3, 5, 7, 8, 10, 12]:
+            days = 31
+        print days
+        self.create_dates(days, month)
+
+    def create_dates(self, days, month):
+        for day in range(1, days + 1):
+            date = Date(day=day, month=month)
+            date.write_into(self.dates)
+
+
 class Person:
     def __init__(self, name, birth_date):
         self.name = name
